1ef31119fbc95f0d54e57f1fff49db2eb846de5d
jukyokim <jukyokim1992@gmail.com>
2015-12-11 09:10:02 -0500

    comments stuff now

--- controllers/postCtrl.js
+++ controllers/postCtrl.js
@@ -1,12 +1,40 @@
 var Post = require('../helpers/postHelp.js');
 
 module.exports.controller = function(app) {
-	//grab all posts in the database(including info)
-	app.get('/posts',function(req,res){
-			console.log(arguments);
+	
+	//grab all blog posts
+	app.get('/post',function(req, res){
 		Post.grabAllBlogs(function(posts){
-			res.send(posts);
-
+			res.send(posts)
 		});
 	});
-};
+	
+	//grab single blog post
+	app.get('/post/:id',function(req,res){
+		Post.grabSpecificPost(req.params.id, function(post){
+			res.send(post);
+		});
+	});
+
+	//update specifc post
+	app.patch('/post/:id',function(req,res){
+		Post.updateSpecificPost(req.params.id, req.body.title, req.body.text, function(post){
+			res.send(post);
+		});
+	});
+
+	//delete that post
+	app.delete('/post/:id',function(req,res){
+		Post.deletePost(req.params.id, function(message){
+			res.send(message);
+		});
+	});
+
+	//add comments
+	app.post('/post/:id/commentEntry',function(req,res){
+		Post.addComment(req.params.id, req.body, function(returned){
+			res.send(returned)
+		});
+	});
+
+}/* end of the function */
--- helpers/postHelp.js
+++ helpers/postHelp.js
@@ -1,4 +1,5 @@
 var Post = require('../model/postModel.js');
+var Comments = require('../model/commentModel.js');
 
 module.exports = {
 	grabAllBlogs:function(callback){
@@ -9,6 +10,59 @@ module.exports = {
 				callback(post);
 			}
 		});
-	}/*end of the function*/
+	},/*end of the function*/
+
+	grabSpecificPost:function(id, callback){
+		Post.find({_id: id}).exec(function(err, post){
+			callback(post);
+		});
+	},/*end of the function*/
+
+	updateSpecificPost:function(id, title, inputText, callback){
+		Post.find({_id: id}).exec(function(err, returned){
+			if(err){
+				console.log(err);
+			}else{
+				title === "" ? title = returned[0].postTitle: title = title;
+				inputText === "" ? inputText = returned[0].text: inputText;
+				Post.update({_id: id}, { $set: { postTitle:title, text:inputText }},function(err,post){
+					if(err){
+						console.log(err);
+					}else{
+						callback(post);
+					}
+				});
+			}
+		});
+	},/*end of the function*/
+
+	deletePost:function(id, callback){
+		Post.remove({_id: id}).exec(function(err){
+			err ? console.log(err) : callback("post has been deleted");
+		});
+	},/*end of the function*/
+
+	addComment:function(id, commentObj, callback){
+		Post.find({_id: id}).populate('comment').exec(function(err, returnedBlog){
+			var comment = new Comments(commentObj);
+			comment.save(function(err){
+				if(err){
+					console.log(err);
+				}else{
+					returnedBlog[0].commentsPosts.push(comment._id);
+					returnedBlog[0].save(function(err){
+						err ? console.log(err): callback(returnedBlog[0]);
+					});
+				}
+			});
+		});
+	},/*end of the function*/
+
+	grabBlogComments:function(){
+		Post.find({_id: id}).exec(function(err, returnedPost){
+			returnedPost[0]
+		});
+	}
+
 
 };
